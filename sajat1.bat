88c3c6edc7b3c853adfabf0f5e1b3644861174f6
MegoltElek <78854609+SzollosiJanos@users.noreply.github.com>
2023-03-13 13:21:57 +0100

    Add files via upload

--- sajat1.bat
+++ sajat1.bat
@@ -1,3 +1,3 @@
 SET PATH=%PATH%;%cd%\MinGW\bin
-gcc BC6X4X.c -o BC6X4X.exe -lOpenCL && BC6X4X.exe C:/Users/szoll/Desktop/szakdolgozat/c_sdk_220203/test/ 2 1
+gcc BC6X4X.c -o BC6X4X.exe -lOpenCL && BC6X4X.exe ./test/ 2 1
 cmd
