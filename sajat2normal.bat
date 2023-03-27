1aa04d617aa94828cb9129d54feff40f8bd96a9d
MegoltElek <78854609+SzollosiJanos@users.noreply.github.com>
2023-03-27 02:11:50 +0200

    Add files via upload

--- sajat2normal.bat
+++ sajat2normal.bat
@@ -1,3 +1,3 @@
 SET PATH=%PATH%;%cd%\MinGW\bin
-gcc BC6X4X_normal.c -o BC6X4X_normal.exe -lOpenCL && BC6X4X_normal.exe ./test/ 2 2
+gcc BC6X4X_normal.c -o BC6X4X_normal.exe && BC6X4X_normal.exe ./test/ 2 2
 cmd
